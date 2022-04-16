73661711c42d50005180100e1960bc11ec64f9e1
Zahra Dargahi <zahra.dargahi@iag.com.au>
2022-04-17 09:38:53 +1000

    adding routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, Outlet } from "react-router-dom";
+import Home from './routes/home/home.component';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+
+
+    const Shop = () => {
+        return(
+            <div>
+                <h1> I am shop</h1>
+            </div>
+        )
+    }
+    return  (
+        <Routes>
+            <Route path="/" element={<Navigation />}>
+                <Route path="/shop" element = {<Shop />}/>
+                <Route  index element = {<Home />} />
+            </Route>
+        </Routes>
+    )
+};
 
 export default App;
